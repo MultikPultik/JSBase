d2c60deebafbc2d31c26b35f6e92323c6a2e90cf
AvisRara <tsiganov.da@gmail.com>
2020-12-30 17:13:03 +0400

    Завершена задача 1

--- script.js
+++ script.js
@@ -1,87 +1,96 @@
 "use strict";
 
-const productDesc = {
-    render(product) {
-        return `<div class="productWrap">
-            <img src="${product.productImg}" alt="picture">
-            <div><b>Наименование</b>: ${product.productName}</div>
-            <div><b>Цена</b>: ${product.productPrice}</div>
-            <button class="btnCatalogItem" data-product_id="${product.productId}">Купить</button>
-        </div>`;
-    }
-};
-
-
+/**
+ *  Объект корзины
+ */
 let catalog = {
     catalogDiv: null,
-    productDesc,
+    basket: {},
 
-    products: [
+    basketProducts: [
         {
             productId: 10,
             productName: 'Телевизор LG',
             productPrice: 25000,
-            productImg: 'https://picsum.photos/id/237/300',
+            productImg: "img/TV.jpg",
         },
         {
             productId: 20,
             productName: 'Держатель для телевизора',
             productPrice: 1000,
-            productImg: 'https://picsum.photos/id/241/300',
+            productImg: "img/fixingPart.jpg",
         },
         {
             productId: 30,
             productName: 'Батарейки для пульта',
-            productImg: 'https://picsum.photos/id/243/300',
+            productImg: "img/battary.jpg",
             productPrice: 60,
         },
         {
             productId: 40,
             productName: 'Подписка "Русское кино"',
             productPrice: 680,
-            productImg: 'https://picsum.photos/id/242/300',
+            productImg: "img/subscribe.jpg",
         },
     ],
 
-    init() {
+    /* 
+     * Метод инициальзации каталога
+     * @param basket - указатель на корзину 
+    */
+    init(basket) {
         this.catalogDiv = document.getElementById("catalog");
+        this.basket = basket;
         this.render();
         this.setEventHandlers();
     },
 
+    /* 
+     * Метод визуализации каталога
+    */
     render() {
-        if (this.products.length) {
-            this.products.forEach(product => {
-                this.catalogDiv.insertAdjacentHTML('beforeend', this.productDesc.render(product));
+        if (this.basketProducts.length) {
+            this.basketProducts.forEach(product => {
+                this.catalogDiv.insertAdjacentHTML('beforeend', this.contentItemHTML(product));
             });
         }
     },
 
+    /* 
+     * Метод обработки событий
+    */
     setEventHandlers() {
         this.catalogDiv.addEventListener('click', event => this.addToBasket(event));
     },
 
-    addToBasket(event){
+    /* 
+     * Метод добавления в корзину товара
+    */
+    addToBasket(event) {
         if (event.target.classList.contains('btnCatalogItem')) {
             const product_id = +event.target.dataset.product_id;
-            console.log(product_id);
+            // console.log(product_id);
+            this.basket.addToBasket(product_id);
         } else {
             return;
         }
 
     },
-};
 
-const basketItem = {
-    render(product) {
-        return `<div class="product">
-            <div><b>Наименование</b>: ${product.productName}</div>
-            <div><b>Цена за шт.</b>: ${product.productPrice}</div>
-            <div><b>Количество</b>: ${product.amount}</div>
-            <div><b>Цена</b>: ${product.amount * product.productPrice}</div>
-        </div>`;
+    /* 
+    * Метод добавления разметки для представления товара
+    */
+    contentItemHTML(product) {
+
+        return `<div class="productWrap">
+                <img src="${product.productImg}" alt="picture">
+                <div><b>Наименование</b>: ${product.productName}</div>
+                <div><b>Цена</b>: ${product.productPrice} руб.</div>
+                <button class="btnCatalogItem" data-product_id="${product.productId}">Купить</button>
+            </div>`;
     }
 };
+
 
 /**
  *  Объект корзины
@@ -89,47 +98,89 @@ const basketItem = {
 let basket = {
     basketWrap: null,
     basketBtn: null,
-    basketItem,
+    basketDescr: null,
 
     catalogList: [],
-    products: [],
+    basketProducts: [],
 
+
+    /* 
+     * Метод инициальзации корзины
+     * @param catalogList - список товаров в каталоге 
+    */
     init(catalogList) {
         this.basketWrap = document.querySelector(".basketWrap");
         this.basketBtn = document.querySelector(".basketBtn");
+        this.basketDescr = document.querySelector(".basketTotalDescr");
         this.catalogList = catalogList;
-        
+
         this.basketBtn.addEventListener('click', () => {
             this.clearBasket();
         });
         this.render();
     },
 
+    /**
+     * Метод очистки корзины
+     */
+    clearBasket() {
+        this.basketProducts = [];
+        this.render();
+    },
+
+    /**
+     * Метод визуализации корзины
+     */
     render() {
-        if (this.products.length) {
-           /*  this.products.forEach(product => {
-                this.basketWrap.insertAdjacentHTML('beforeend', this.basketItem.render(product));
+        if (this.basketProducts.length > 0) {
+            this.basketWrap.innerHTML = '';
+            this.basketProducts.forEach(product => {
+                this.basketWrap.insertAdjacentHTML('beforeend', this.contentItemHTML(product));
             });
-            this.basketWrap.insertAdjacentHTML('beforeend', `В корзине ${this.products.length} товара стоимостью ${this.getSumPrice()}`); */
+            this.basketDescr.innerHTML = '';
+            this.basketDescr.insertAdjacentHTML('beforeend', `<br><br>В корзине ${this.basketProducts.length} товар(а,-ов) стоимостью ${this.getSumPrice()} руб.`);
         } else {
+            this.basketDescr.innerHTML = '';
             this.basketWrap.textContent = 'Корзина пуста';
         }
     },
 
-    getSumPrice() {
-        return this.products.reduce(function (productPrice, product) {
-            return productPrice + product.productPrice * product.amount;
-        }, 0);
-    },
-    
-    clearBasket() {
-        this.products = [];
+    /**
+     * Метод добавления товара в корзину из каталога
+     */
+    addToBasket(product_id) {
+        const product = this.catalogList.find(product => product.productId === product_id);
+
+        if (product) {
+            this.basketProducts.push({ ...product })
+        }
         this.render();
     },
+
+    /**
+     * Метод подсчета количество товаров в корзине и их стоимости
+     */
+    getSumPrice() {
+        return this.basketProducts.reduce(function (productPrice, product) {
+            return productPrice + product.productPrice;
+        }, 0);
+    },
+
+    /* 
+     * Метод добавления разметки для представления товара
+    */
+    contentItemHTML(product) {
+
+        return `<div class="productWrap">
+        <img src="${product.productImg}" alt="picture">
+        <div><b>Наименование</b>: ${product.productName}</div>
+        <div><b>Цена</b>: ${product.productPrice} руб.</div>
+        </div>`;
+    }
 };
 
-catalog.init();
-basket.init();
+catalog.init(basket);
+basket.init(catalog.basketProducts);
 
 
 
